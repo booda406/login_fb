65f2e8fbbe3c0fa62d1cea3d9f679368c3b62560
Steven Tseng <booda406@gmail.com>
2013-11-18 19:06:23 +0800

    chg root to test_login#index

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ LoginTest::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'test_logins#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
